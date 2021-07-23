b2f6d13b3fe83827fd6a81611026c87696152c62
Anatoly Artamonov <aartamonov@smartling.com>
2021-07-23 11:15:44 +0300

    RPS-5874. Update our Public Python SDK to a Tier 2 spec. Added missing file

---
+++ smartlingApiSdk/ApiV1.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+''' Copyright 2012 Smartling, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this work except in compliance with the License.
+ * You may obtain a copy of the License in the LICENSE file, or at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+'''
+
+#FileApi V1 class implementation
+
+from .HttpClient import HttpClient
+from .MultipartPostHandler import MultipartPostHandler
+from .Constants import Uri, Params, ReqMethod
+from .ApiResponse import ApiResponse
+from .FileApiBase import FileApiBase
+
+class ApiV1(FileApiBase):
+    """ Api v1 basic functionality """
+
+    def commandUpload(self, uploadData):
+        params = {
+                    Params.FILE_URI: uploadData.uri or uploadData.name,
+                    Params.FILE_TYPE: uploadData.type,
+                    Params.FILE_PATH: uploadData.path + uploadData.name
+                  }
+        if (uploadData.approveContent):
+            params[Params.APPROVED] = uploadData.approveContent
+
+        if (uploadData.callbackUrl):
+            params[Params.CALLBACK_URL] = uploadData.callbackUrl
+
+        if (uploadData.directives):
+            for index, directive in enumerate(uploadData.directives):
+                params[directive.sl_prefix + directive.name] = directive.value
+                
+        if (uploadData.localesToApprove):
+            for index, locale in enumerate(uploadData.localesToApprove):
+                params['{0}[{1}]'.format(Params.LOCALES_TO_APPROVE, index)] = locale
+
+        return self.uploadMultipart(Uri.UPLOAD, params)
+
+    def commandList(self, **kw):
+        return self.command(ReqMethod.POST, Uri.LIST, kw)
+
+    def commandLastModified(self, fileUri, locale=None, **kw):
+        kw[Params.FILE_URI] = fileUri
+        if locale is not None:
+            kw[Params.LOCALE] = locale
+        return self.command(ReqMethod.GET, Uri.LAST_MODIFIED, kw)
+        
+    def commandGet(self, fileUri, locale, **kw):
+        kw[Params.FILE_URI] = fileUri
+        kw[Params.LOCALE] = locale
+        if Params.RETRIEVAL_TYPE in kw and not kw[Params.RETRIEVAL_TYPE] in Params.allowedRetrievalTypes:
+            raise "Not allowed value `%s` for parameter:%s try one of %s" % (kw[Params.RETRIEVAL_TYPE],
+                                                                             Params.RETRIEVAL_TYPE,
+                                                                             Params.allowedRetrievalTypes)
+
+        return self.getResponseAndStatus(ReqMethod.POST, Uri.GET, kw)
+
+    def commandDelete(self, fileUri, **kw):
+        kw[Params.FILE_URI] = fileUri
+
+        return self.command(ReqMethod.POST, Uri.DELETE, kw)
+        
+    def commandImport(self, uploadData, locale, **kw):
+        kw[Params.FILE_URI]  = uploadData.uri
+        kw[Params.FILE_TYPE] = uploadData.type
+        kw[Params.FILE_PATH] = uploadData.path + uploadData.name
+        kw["file"] = uploadData.path + uploadData.name + ";type=text/plain"
+        kw[Params.LOCALE] = locale
+
+        return self.uploadMultipart(Uri.IMPORT, kw)
+
+    def commandStatus(self, fileUri, locale, **kw):
+        kw[Params.FILE_URI] = fileUri
+        kw[Params.LOCALE] = locale
+
+        return self.command(ReqMethod.POST, Uri.STATUS, kw)
+
+    def commandRename(self, fileUri, newUri, **kw):
+        kw[Params.FILE_URI] = fileUri
+        kw[Params.FILE_URI_NEW] = newUri
+
+        return self.command(ReqMethod.POST, Uri.RENAME, kw)
